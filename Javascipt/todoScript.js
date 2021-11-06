521e8b7384343cb70456aa20a3fc5d164715e262
JPandor <juwairiah1308@gmail.com>
2021-11-06 18:37:23 +0200

    fixed function
    fixed up the function of writing it to the inner html. Still need to add in the code to properly hide the appropriate divs

--- Javascipt/todoScript.js
+++ Javascipt/todoScript.js
@@ -1,52 +1,57 @@
-// Getting input fields
-let taskName = document.getElementById("task").value;
-let taskDescription = document.getElementById("description").value;
-let taskEndDate = document.getElementById("due_date").value;
-let taskEndTime = document.getElementById("due_time").value;
+
+
 
 class Task {
-    constructor (title, description, due_date, due_time, completed){
+    constructor (title, description, due_date, due_time, completed, id){
         this.title = title;
         this.description = description;
         this.due_date = due_date;
         this.due_time = due_time;
         this.completed = completed;
+        this.id = id;
     };
 
-    defaultCompleted(){
-        this._completed = false;
-    };
 };
 
+function writeToDisplay(){
+    // getting input fields
+    let taskName = document.getElementById("task").value;
+    let taskDescription = document.getElementById("description").value;
+    let taskEndDate = document.getElementById("due_date").value;
+    let taskEndTime = document.getElementById("due_time").value;
+    
+    // creating a container div for each task 
+    let newDiv = document.createElement("div");
+
+    //creating a new list item and setting it to the task title
+    let newList = document.createElement("li");
+    let listInside = document.createTextNode(taskName);
+    newList.appendChild(listInside);
+
+    // creating the check button 
+    let checkBtn = document.createElement("button");
+    checkBtn.innerHTML = "Complete"
+
+    //creating the delete button 
+    let delBtn = document.createElement("button");
+    delBtn.innerHTML = "Delete";
+
+    //creating hidden div with details of task 
+    let hideDiv = document.createElement("div");
+    hideDiv.innerHTML = `Description ${taskDescription} <br> Due Date ${taskEndDate} <br> Due Time ${taskEndTime}`;
 
 
+    // appending all new elements 
+    document.getElementById("taskList").appendChild(newDiv)
+    newDiv.appendChild(newList);
+    newDiv.appendChild(checkBtn);
+    newDiv.appendChild(delBtn);
+    newDiv.appendChild(hideDiv);
 
-function addingToList(){
-    let newTask = new Task (taskName, taskDescription, taskEndDate, taskEndTime, false);
-    return newTask;
+
 }
 
-let string = addingToList();
-string = JSON.stringify(string);
-console.log(string)
 
-function writeToDisplay () {
-    let section = document.createElement("section");
-    section.innerHTML = string;
-    document.getElementById("taskList").appendChild(section);
-}
 
-// function writeToHtml () {
-//     let section = document.createElement("section");
-//     section.innerHTML = newTask.title;
-//     // section.setAttribute("id", "bigDiv")
-//     
-//     // let collapse = document.createElement("div");
-//     // document.getElementById("bigDiv").appendChild(collapse);
-//     // collapse.setAttribute("class", "collapse");
-//     // collapse.setAttribute("id", "bigshot")
-//     // let card = document.createElement("div");
-//     // card.setAttribute("class", "card");
-//     // document.getElementById("bigshot").appendChild(card);
-//     // card.innerHTML = `${newTask._description} <br> ${newTask._due_date} <br> ${newTask._due_time}`;
-// }
+
+
